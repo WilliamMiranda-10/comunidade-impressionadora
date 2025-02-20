3425b6786a367cd0cf04846ef3bd75c2df7b7c8c
WILLIAM MIRANDA AMORIM <willianmiranda127@gmail.com>
2025-02-19 20:55:00 -0400

    Update routes.py

--- comunidadeimpressionadora/routes.py
+++ comunidadeimpressionadora/routes.py
@@ -44,7 +44,7 @@ def login():
         else:
             flash(f'Falha no login. E-mail ou Senha Incorretos', 'alert-danger')
     if form_criar_conta.validate_on_submit() and 'botao_submit_criar_conta' in request.form:
-        senha_cript = bcrypt.generate_password_hash(form_criar_conta.senha.data)
+        senha_cript = bcrypt.generate_password_hash(form_criar_conta.senha.data).decode('utf-8')
         usuario = Usuario(username=form_criar_conta.username.data, email=form_criar_conta.email.data, senha=senha_cript)
         database.session.add(usuario)
         database.session.commit()
